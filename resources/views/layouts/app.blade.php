f87d610f29af83dd1acc9a4181b2a0f801865d78
Muhammad Faried Risky <63082506+fariedrisky12@users.noreply.github.com>
2020-05-15 06:15:01 +0700

    Perubahan Pertama Faried

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,10 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
@@ -24,6 +28,180 @@
         body{
             background-color: #ff1a75;
         }
+
+        .team {
+        min-height: 500px;
+        padding: 60px 15px;
+        float: left;
+        height: 200%;
+        width: 100%;
+        }
+
+        .team h1 {
+        font-size: 36px;
+        font-family: 'Montserrat';
+        font-weight: 700;
+        color: #ffffff;
+        margin: 0;
+        padding-bottom: 60px;
+        text-align: center;
+        }
+
+        .team .card {
+            width: 50%;
+            float: left;
+            padding: 10px 15px;
+            color: #c7c7c7;
+        }
+
+        .team .card .box {
+            padding: 15px;
+            background-color: #ffffff;
+            border-radius: 5px;
+            transition: margin 1s ease, box-shadow 1s ease;
+            color: #3e3e3e;
+            }
+
+
+        .team .card .box img {
+            width: 150px;
+            display: block;
+            margin: 10px auto;
+            border-radius: 50%;
+            color: #ffffff;
+
+}
+
+.team .card .box h4 {
+  font-size: 20px;
+  font-family: 'Montserrat';
+  margin: 20px 0px 10px;
+  color: #31a840;
+  text-align: center;
+  font-weight: 700;
+}
+
+.team .card .box h5 {
+  font-size: 16px;
+  font-family: 'Montserrat';
+  font-weight: 600;
+  color: #b9b9b9;
+  margin: 0px 0px 15px;
+  text-align: center;
+}
+
+.team .card .box p {
+  color: #b9b9b9;
+  font-size: 15px;
+  font-family: 'Montserrat';
+  font-weight: 400;
+  line-height: 24px;
+  text-align: center;
+  padding: 0px 20px;
+}
+
+        .about-section {
+        width: 100%;
+        height: 200%;
+  background: #738299;
+  padding: -100px -100px;
+  font-family: 'Montserrat', sans-serif;
+background-size: 110%;
+
+}
+
+.about H2 {
+  color: #000000;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.about H1 {
+  color: #000000;
+  font-size: 36px;
+  font-family: 'Montserrat';
+  font-weight: 700;
+}
+
+.team .icon b,
+
+.inner-width {
+  max-width: 1000px;
+  overflow: hidden;
+  padding: 0 20px;
+  margin: auto;
+}
+
+.about-section h1 {
+  text-align: center;
+}
+
+.border {
+  width: 100px;
+  height: 3px;
+  background: grey;
+  margin: 48px auto;
+}
+
+.about-section-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.about-section-col {
+  flex: 50%;
+}
+
+.about {
+  padding-right: 30px;
+}
+
+.about p {
+  text-align: justify;
+  margin-bottom: 20px;
+}
+
+.about a {
+  display: inline-block;
+  color: #e74c3c;
+  text-decoration: none;
+  border: 2px solid #e74c3c;
+  border-radius: 24px;
+  padding: 8px 40px;
+  transition: 0.4s linier;
+}
+
+.about a:hover {
+  color: #fff;
+  background: #e74c3c;
+}
+
+@media screen and (max-width: 700px) {
+  .about-section-col {
+    flex: 100%;
+    margin: 100px 0;
+  }
+
+  .about,
+  .skills {
+    padding: 0;
+  }
+
+  .about {
+    text-align: center;
+  }
+
+  .email{
+	  font-family: 'Montserrat';
+  }
+  
+  .instagram{
+ig:	font-family: 'Montserrat';
+  }
+
+  .box{
+	  color: #ffffff;
+  }
+}
     </style>
 
 </head>
@@ -50,7 +228,7 @@
                             <a class="nav-link" href="/">HOME</a>
                         </li>
                         <li class="nav-item active m-3" >
-                            <a class="nav-link" href="">TENTANG KAMI</a>
+                            <a class="nav-link" href="/team-section">TENTANG KAMI</a>
                         </li>
 
                         <!-- Authentication Links -->
