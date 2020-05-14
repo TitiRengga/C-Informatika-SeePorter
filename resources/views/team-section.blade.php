5f6571fed43a2b08e228894b91f97ed49b3b5dda
Muhammad Faried Risky <63082506+fariedrisky12@users.noreply.github.com>
2020-05-15 06:08:41 +0700

    Perubahan Pertama Faried

---
+++ resources/views/team-section.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<section class="about">
+		<div class="about-section">
+			<div class="inner-width">
+				<h1> Tentang Kami </h1>
+				<div class="border"></div>
+				<div class="about-section-row">
+					<div class="about-section-col">
+						<div class="about">
+							<h3 style="text-align: justify;">
+							SeePorter ialah website yang dapat membantu pengguna dan porter dengan fungsi yang berbeda. 
+							SeePorter dirancang untuk membantu siapapun yang kesusahan mengangkat barang belanjaan di pasar.
+							</h3>
+						</div>
+					</div>
+					<section class="team">
+						<div class="container">
+							<h1>Tim Kami</h1>
+							<div class="card">
+								<div class="box">
+									<img src="img/one.jpg" alt="team img" />
+									<h4>Hadi Shirath Maulana</h4>
+									<h5 style="text-align: center;">
+										<b class="instagram">ig: hadi.shirath</b>
+									</h5>
+									<p>
+										<b class="email"> hadishirathmaulana@gmail.com</b>
+									</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="box">
+									<img src="img/two.jpg" alt="team img" />
+									<h4>Titi Rengga Sari</h4>
+									<h5 style="text-align: center;">
+										<b class="instagram">ig: titirengga_sari</b>
+									</h5>
+									<p>
+										<b class="email"> titirengga5514@gmail.com</b>
+									</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="box">
+									<img src="img/three.jpg" alt="team img" />
+									<h4>Muhammad Faried Risky</h4>
+									<h5 style="text-align: center;">
+										<b class="instagram">ig: fariedrisky</b>
+									</h5>
+									<p>
+										<b class="email"> fariedrisky@gmail.com</b>
+									</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="box">
+									<img src="img/four.jpg" alt="team img" />
+									<h4>Hijaiyah Imanda Putri</h4>
+									<h5 style="text-align: center;">
+										<b class="instagram">ig: imanda23</b>
+									</h5>
+									<p>
+										<b class="email"> imandaputri@gmail.com</b>
+									</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="box">
+									<img src="img/five.jpg" alt="team img" />
+									<h4>Hafizh Alhadi</h4>
+									<h5 style="text-align: center;">
+										<b class="instagram">ig: hafizh_alhadi</b>
+									</h5>
+									<p>
+										<b class="email"> hafizhjackmania@gmail.com</b>
+									</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="box">
+									<img src="img/six.jpg" alt="team img" />
+									<h4>Ilham Syahputra</h4>
+									<h5 style="text-align: center;">
+										<b class="instagram">ig: ilhamputra</b>
+									</h5>
+									<p>
+										<b class="email"> putrailham1806@gmail.com</b>
+									</p>
+								</div>
+							</div>
+
+						</div>
+
+					</section>
+@endsection
