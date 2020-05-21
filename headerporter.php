77f8f00c948dccb8fee28a0fde5757ed5da5e539
Hijaiyah Imanda Putri <63409852+imandaputri@users.noreply.github.com>
2020-05-22 06:07:46 +0700

    homeuser kedua

---
+++ headerporter.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand container" href="#">
+      <img src="logo.png" width="500" height="150" alt="See Porter"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link text-white " href="index.html">Riwayat Chat<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white " href="#">Akun</a>
+        </li>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white " href="#">Tentang Kami</a>
+      </li>
+      </ul>
+    </div>
+  </nav>
---
+++ homeporter.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- MY CSS -->
+    <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500');
+
+.navbar-brand{
+    font-family : 'Franklin Gothic Medium';
+    font-size : 60px;
+}
+
+.jumbotron {
+    margin-top: -200px;
+}
+
+.jarak{
+  margin-left: 240px;
+}
+
+.jumbotron .display-4{
+    text-align: center;
+    margin-top: 210px;
+    font-weight: 500;
+    color: #5c5c5c;
+    font-family: 'arial';
+    font-size:50px;
+}
+
+.tengah{
+  display:flex;
+  align-items:center;
+  justify-content: center;
+}
+
+.Kotak{
+  text-align: center;
+  margin-left: 40px;
+  margin-top: -300px;
+  background-color : #179774;
+  font-family : 'arial';
+  width :400px;
+  height: 200px;
+  font-weight: 500;
+}
+
+.jumbotron .lead{
+    text-align: center;
+    font-weight: 500;
+    color: #5c5c5c;
+    font-family: 'arial';
+}
+
+.nav-link{
+    text-transform: uppercase;
+    text-align: center;
+    margin-right: 20px;
+    margin-top: -3px;
+    border-radius: 10px;
+    background-color : #179774;
+    font-family : 'Franklin Gothic Medium';
+    width :170px;
+}
+</style>
+
+    <title>See Porter</title>
+  </head>
+  <body>
+
+      <?php include 'headerporter.php'; ?>   
+    
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container jarak">
+        <h1 class="display-4 ml-auto">AWALI HARIMU<br>DENGAN BISMILLAH<br>
+        <font color="#179774"><b>PORTER</b></font></h1>
+        
+      </div>
+    </div>
+    <div class="Kotak text-white tengah"><h5><b>Nikmatilah setiap pekerjaan<br>yang kamu dapatkan,<br>karena tidak semua orang<br>memperoleh kesempatan<br>yang sama</b></h5></div>
+
+     
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
