d5875e06045a29330c5f1689ae76cc93ed24cf82
Titi Rengga Sari <63796026+TitiRengga@users.noreply.github.com>
2020-05-28 23:27:24 +0700

    perubahan keempat titi

--- resources/views/team-section.blade.php
+++ resources/views/team-section.blade.php
@@ -1,98 +1,147 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="services-section">
+    <div class="inner-width">
+		<h1 class="section-title">3 pilar SeePorter</h1>
+		<div class="border" style="margin-top:-20px; width:30vh"></div>
+        <div class="services-container">
 
-	<section class="about">
-		<div class="about-section">
-			<div class="inner-width">
-				<h1> Tentang Kami </h1>
-				<div class="border"></div>
-				<div class="about-section-row">
-					<div class="about-section-col">
-						<div class="about">
-							<h3 style="text-align: justify;">
-							SeePorter ialah website yang dapat membantu pengguna dan porter dengan fungsi yang berbeda. 
-							SeePorter dirancang untuk membantu siapapun yang kesusahan mengangkat barang belanjaan di pasar.
-							</h3>
-						</div>
-					</div>
-					<section class="team">
-						<div class="container">
-							<h1>Tim Kami</h1>
-							<div class="card">
-								<div class="box">
-									<img src="img/one.jpg" alt="team img" />
-									<h4>Hadi Shirath Maulana</h4>
-									<h5 style="text-align: center;">
-										<b class="instagram">ig: hadi.shirath</b>
-									</h5>
-									<p>
-										<b class="email"> hadishirathmaulana@gmail.com</b>
-									</p>
-								</div>
-							</div>
-							<div class="card">
-								<div class="box">
-									<img src="img/two.jpg" alt="team img" />
-									<h4>Titi Rengga Sari</h4>
-									<h5 style="text-align: center;">
-										<b class="instagram">ig: titirengga_sari</b>
-									</h5>
-									<p>
-										<b class="email"> titirengga5514@gmail.com</b>
-									</p>
-								</div>
-							</div>
-							<div class="card">
-								<div class="box">
-									<img src="img/three.jpg" alt="team img" />
-									<h4>Muhammad Faried Risky</h4>
-									<h5 style="text-align: center;">
-										<b class="instagram">ig: fariedrisky</b>
-									</h5>
-									<p>
-										<b class="email"> fariedrisky@gmail.com</b>
-									</p>
-								</div>
-							</div>
-							<div class="card">
-								<div class="box">
-									<img src="img/four.jpg" alt="team img" />
-									<h4>Hijaiyah Imanda Putri</h4>
-									<h5 style="text-align: center;">
-										<b class="instagram">ig: imanda23</b>
-									</h5>
-									<p>
-										<b class="email"> imandaputri@gmail.com</b>
-									</p>
-								</div>
-							</div>
-							<div class="card">
-								<div class="box">
-									<img src="img/five.jpg" alt="team img" />
-									<h4>Hafizh Alhadi</h4>
-									<h5 style="text-align: center;">
-										<b class="instagram">ig: hafizh_alhadi</b>
-									</h5>
-									<p>
-										<b class="email"> hafizhjackmania@gmail.com</b>
-									</p>
-								</div>
-							</div>
-							<div class="card">
-								<div class="box">
-									<img src="img/six.jpg" alt="team img" />
-									<h4>Ilham Syahputra</h4>
-									<h5 style="text-align: center;">
-										<b class="instagram">ig: ilhamputra</b>
-									</h5>
-									<p>
-										<b class="email"> putrailham1806@gmail.com</b>
-									</p>
-								</div>
-							</div>
+        <div class="service-box">
+            <div class="service-icon">
+            <i class="far fa-smile-beam"></i>
+            </div>
+            <div class="service-title">Kepuasan Pengguna</div>
+            <div class="service-desc">
+            Kepuasan pengguna merupakan prioritas utama dari website SeePorter.
+            </div>
+        </div>
 
-						</div>
+        <div class="service-box">
+            <div class="service-icon">
+            <i class="fas fa-money-bill"></i>
+            </div>
+            <div class="service-title">tarif yang terjangkau</div>
+            <div class="service-desc">
+			Kami memberikan tarif setiap layanan yang diberikan cocok dengan semua kalangan.
+            </div>
+		</div>
 
-					</section>
+		<div class="service-box">
+            <div class="service-icon">
+            <i class="fas fa-shipping-fast"></i>
+            </div>
+            <div class="service-title">Kecepatan</div>
+            <div class="service-desc">
+            Kami melayani dengan cepat dan akan terus meningkatkan pelayanan yang diberikan.
+            </div>
+		</div>
+	</div>
+</div>
+
+<section class="team-section">
+    <div class="container-team">
+        <div class="row">
+            <div class="section-title">
+                <h1>Anggota Tim</h1>
+            </div>
+            <div class="border" style="margin-top:-60px; width:30vh"></div>
+        </div>
+        <div class="row">
+            <div class="team-items">
+                <div class="item">
+                    <img src="img/one.jpg" alt="team" />
+                    <div class="inner">
+                        <div class="info">
+                            <h5>Hadi Shirath Maulana</h5>
+                            <p>Designer</p>
+                            <div class="social-links">
+                                <a href="https://www.facebook.com/hadi.shirath"><span class="fa fa-facebook"></span></a>
+                                <a href="https://github.com/HadiShirathMaulana"><span class="fa fa-github"></span></a>
+                                <a href="https://instagram.com/hadi.shirath?igshid=1gzqzivqao4y9"><span class="fa fa-instagram"></span></a>
+                                <a href=""><span class="fa fa-youtube"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="img/two.jpg" alt="team" />
+                    <div class="inner">
+                        <div class="info">
+                            <h5>Titi Rengga Sari</h5>
+                            <p>Designer</p>
+                            <div class="social-links">
+                                <a href="https://www.facebook.com/titi.rengga.3"><span class="fa fa-facebook"></span></a>
+                                <a href="https://github.com/TitiRengga"><span class="fa fa-github"></span></a>
+                                <a href="https://instagram.com/titirengga_sari?igshid=djtlinryqhwc"><span class="fa fa-instagram"></span></a>
+                                <a href="https://www.youtube.com/channel/UC5Yx07ueMW3ZAYN98_VtnNw"><span class="fa fa-youtube"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="img/three.jpg" alt="team" />
+                    <div class="inner">
+                        <div class="info">
+                            <h5>M. Faried Risky</h5>
+                            <p>Designer</p>
+                            <div class="social-links">
+                                <a href="http://facebook.com/fariedrisky"><span class="fa fa-facebook"></span></a>
+                                <a href="https://github.com/fariedrisky12"><span class="fa fa-github"></span></a>
+                                <a href="https://instagram.com/fariedrisky?igshid=1pcxqw24pixc1"><span class="fa fa-instagram"></span></a>
+                                <a href="https://www.youtube.com/channel/UCtOjxpklb4Ix2BOUsfPQ0AQ"><span class="fa fa-youtube"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="img/four.jpg" alt="team" />
+                    <div class="inner">
+                        <div class="info">
+                            <h5>Hijaiyah Imanda Putri</h5>
+                            <p>Designer</p>
+                            <div class="social-links">
+                                <a href="https://www.facebook.com/imanda.putri.1"><span class="fa fa-facebook"></span></a>
+                                <a href="https://github.com/imandaputri"><span class="fa fa-github"></span></a>
+                                <a href="https://instagram.com/imanda.23?igshid=s0h4mvmxzvq5"><span class="fa fa-instagram"></span></a>
+                                <a href="https://www.youtube.com/channel/UCqrK1kUNSj7zTn_0evwqzvw"><span class="fa fa-youtube"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="img/five.jpg" alt="team" />
+                    <div class="inner">
+                        <div class="info">
+                            <h5>Hafizh Al-Hadi</h5>
+                            <p>Designer</p>
+                            <div class="social-links">
+                                <a href="https://www.facebook.com/hafizh.a.hadi"><span class="fa fa-facebook"></span></a>
+                                <a href="https://github.com/Hafizh31"><span class="fa fa-github"></span></a>
+                                <a href="https://instagram.com/hafizh_alhadi?igshid=1bq1o1b895z1y"><span class="fa fa-instagram"></span></a>
+                                <a href=""><span class="fa fa-youtube"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="img/six.jpg" alt="team" />
+                    <div class="inner">
+                        <div class="info">
+                            <h5>Ilham Syaputra</h5>
+                            <p>Designer</p>
+                            <div class="social-links">
+                                <a href="https://www.facebook.com/profile.php?id=100027549525980"><span class="fa fa-facebook"></span></a>
+                                <a href="https://github.com/putrailham18"><span class="fa fa-github"></span></a>
+                                <a href=""><span class="fa fa-instagram"></span></a>
+                                <a href=""><span class="fa fa-youtube"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
